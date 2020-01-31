c6fd063de99d98fee25c6ded9ad024a2666946e4
molod <molodoj88@gmail.com>
2020-01-31 15:44:46 +0400

    added test data loading into database

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -3,5 +3,6 @@
 mkdir /usr/src/app/database
 python manage.py migrate
 python manage.py collectstatic --no-input --clear
+python manage.py parse "https://www.dropbox.com/s/1yc3rcw3nteyieb/apache_logs_1.txt?dl=1"
 
 exec "$@"
